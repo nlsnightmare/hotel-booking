fd788ba21960ee73cb36246883b95821c4734a30
pchronos1 <p.chronos@icloud.com>
2017-12-28 15:11:16 +0200

    creatining class Booking

--- src/org/teipir/ml/booking/models/Booking.java
+++ src/org/teipir/ml/booking/models/Booking.java
@@ -1,5 +1,106 @@
 package org.teipir.ml.booking.models;
 
 public class Booking {
+	
+	private int bookingID;
+	private String telephone;
+	private String creditCard;
+	private String name;
+	private String surname;
+	private Date bookDate;
+	private Date startingDate;
+	private Date finishDate;
+	private int prepaidAmount;
+	private Room room;
+	private boolean isOnline;
+	
+	
+	
+	public int getbookingID() {
+		return bookingID;
+	}
+	
+	public int setbookingID() {
+		return bookingID;
+	}
 
+	
+	public String gettelephone() {
+		return telephone;
+	}
+	
+	public String settelephone() {
+		return telephone;
+	}
+	
+	public String creditCard() {
+		return telephone;
+	}
+	
+	public String setname() {
+		return name;
+	}
+	
+	public int getname() {
+		return name;
+	}
+	
+	public int setsurname() {
+		return surname
+	
+	public int getsurname() {
+		return surname;
+	}
+	
+	public int setbookDate() {
+		return bookDate;
+	}
+	
+	public int getbookDate() {
+		return bookDate;
+	}
+	
+	public int setstartingDate() {
+		return startngDate;
+	}
+	
+	public int getstartinDate() {
+		return startingDate;
+	}
+	
+	public int setfinishDate() {
+		return finishDate;
+	}
+	
+	public int getfinishDate() {
+		return finishDate;
+	}
+	
+	public int setpaidAmount() {
+		return paidAmount(;
+	}
+	
+	public int getpaidAmount(() {
+		return paidAmount(;
+	}
+	
+	public int setroom() {
+		return room;
+	}
+	
+	public int getroom() {
+		return room;
+	}
+	
+	public int setisOnline() {
+		return bookingID;
+	}
+	
+	public int getbookingID() {
+		return isOnline;
+	}
+	
+	public int setisOnline() {
+		return isOnline;
+	}
 }
