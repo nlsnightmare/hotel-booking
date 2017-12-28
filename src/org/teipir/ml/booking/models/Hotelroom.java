37a9d5ad3baa1562845740bd849c7f6b0a185ed0
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2017-12-28 15:00:10 +0200

    Started working on HotelRoom class

--- src/org/teipir/ml/booking/models/Hotelroom.java
+++ src/org/teipir/ml/booking/models/Hotelroom.java
@@ -1,5 +1,71 @@
 package org.teipir.ml.booking.models;
 
-public class Hotelroom {
+import java.sql.Date;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Hotelroom {
+	private int roomID;
+	private int numberOfBeds;
+	private boolean isStudio;
+	private int numberOfMeals;
+	private int[] priceWithMeals;
+	
+	public int calculatePrice(int numOfMeals, int days) {
+		return priceWithMeals[numOfMeals] * days;
+	}
+	
+	public boolean checkAvailability(Date from, Date till) {
+		throw new NotImplementedException();
+	}
+
+
+	public int getNumberOfBeds() {
+		return numberOfBeds;
+	}
+
+
+	public void setNumberOfBeds(int numberOfBeds) {
+		this.numberOfBeds = numberOfBeds;
+	}
+
+
+	public int getRoomID() {
+		return roomID;
+	}
+
+
+	public void setRoomID(int roomID) {
+		this.roomID = roomID;
+	}
+
+
+	public boolean isStudio() {
+		return isStudio;
+	}
+
+
+	public void setStudio(boolean isStudio) {
+		this.isStudio = isStudio;
+	}
+
+
+	public int getNumberOfMeals() {
+		return numberOfMeals;
+	}
+
+
+	public void setNumberOfMeals(int numberOfMeals) {
+		this.numberOfMeals = numberOfMeals;
+	}
+
+
+	public int[] getPriceWithMeals() {
+		return priceWithMeals;
+	}
+
+
+	public void setPriceWithMeals(int[] priceWithMeals) {
+		this.priceWithMeals = priceWithMeals;
+	}
 }
